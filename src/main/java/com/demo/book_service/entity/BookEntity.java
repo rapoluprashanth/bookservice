937026f626bedbb7493c308fd301b7682528b28e
rapoluprashanth <rapoluprashanth123@gmail.com>
2024-10-08 14:23:58 +0530

    added

--- src/main/java/com/demo/book_service/entity/BookEntity.java
+++ src/main/java/com/demo/book_service/entity/BookEntity.java
@@ -4,13 +4,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@NoArgsConstructor
-@AllArgsConstructor
-@Data
 
 @Entity
 @Table(name="book")
